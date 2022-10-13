b22ce870a07af320796a80b5487693623c5c4776
AnnieOhlen <annie.ohlen@outlook.com>
2022-10-13 12:54:57 +0200

    Pseudokod

--- Program.cs
+++ Program.cs
@@ -6,13 +6,27 @@
         {
             //Grupupgifft Blackjack
 
-
-
-
-
-
-
-
+            /*Skriva välkomnstmeddelande
+                * while (menyval != "4")
+                * skriv ut meny
+                * Läs in menyval
+                * switch meny
+                * case 1 spela
+                * case 2 visa senaste vinnare
+                * case 3 visa spelets regler
+                * case 4 avsluta spelet
+                * default
+                * 
+                * Case 1 frågar vi om spelaren vill dra ett kort j/n väljer spelaren att dra ett kort så slumpas
+                * ett kort mellan 1-10 och då får spelaren reda på vad han fick samt hur mycket poäng hen har,
+                * väljer spelaren att inte dra ett kort ska datorn dra ett kort.
+                * 
+                * Case 2 så behöver vi ha en variabel som sparar spelaren som har vunnit.
+                * Default som tar upp tar upp felinmatning
+                * 
+                * 
+                * 
+                */
         }
     }
 }
